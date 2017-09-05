326cfbb11ead4a04cb26cf853c7761ad54d92f24
omkar.p <omkar.p>
2017-09-05 12:40:26 +0530

    sorting and units solved

--- es6/splitTab.js
+++ es6/splitTab.js
@@ -46,22 +46,20 @@ define(['app','jquery','dataFactory'],function(app,$) {
 					if(data.metrics.length===0){
 						data.metrics=["Impressions Delivered (HB Rendered Ad)"];
 						data.orderingMetric="Impressions Delivered (HB Rendered Ad)";
-						
-					}
-					else{
-						
 					}
 					var container=document.createElement('div');
 					var dim=data['dimensionObjectList'];
 					if(dim.length!=0){
 						dataFactory.getTotalData(data).then(function(response) {
-							console.log(response);
+							//console.log(response);
 							var scope=$scope.$parent;
 							scope.result=response.data.result;
 							if(scope.result[data.metrics[0]]){
 								var x =dataFactory.createSplit(scope.result.split,scope.split.dimensions,'result.split',0);
 						 		var d=$compile(x)(scope)
+						 		$('.tabData').remove();
 						 		for(var i=0;i<d.length;i++){
+						 			//var first=iElm[0].children[0];
 						 			iElm[0].appendChild(d[i]);
 						 		}	
 							}
@@ -70,7 +68,7 @@ define(['app','jquery','dataFactory'],function(app,$) {
 							}
 					 		//console.log(x,d,d.length);								
 						}, function(response){
-							console.log(data);
+							//console.log(data);
 							var scope=$scope.$parent;
 							scope.split.status=2;
 							console.log("error");
